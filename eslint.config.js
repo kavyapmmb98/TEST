3c1e31e76473da19532d8a726fb3738246554462
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-16 15:41:10 +0530

    more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,26 @@ export default [
     {
       files: ["**/*.js"],
       rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "double"],
+        // Possible Errors
+        "no-console": "warn",
+        "no-debugger": "error",
+        "no-extra-semi": "error",
+        "no-irregular-whitespace": "error",
+        "no-unsafe-negation": "error",
+        
+        "curly": "error",
+        "eqeqeq": "error",
+        "no-eval": "error",
+        "no-implicit-globals": "error",
+        "no-multi-spaces": "error",
+        "no-new": "error",
+        "no-unused-expressions": "error",
+  
+        // Variables
+        "no-undef": "error",
+        "no-unused-vars": "error",
+        "no-use-before-define": "error",
+  
       },
     },
   ];
