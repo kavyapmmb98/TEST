f403a086eafe9fc8df2d5ac45439eaec2424c6f9
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-17 17:49:16 +0530

    test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// index.js
+
 
 console.log("Hello, World!");
 
 export function printFirstTenNumbers() {
     for (let i = 1; i <= 10; i++) {
-        console.log(i); // Output numbers without quotes
+        console.log(i); 
     }
 }
 // printFirstTenNumbers()
