356b42b9147e316108b34623075a07e3acff69d0
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-18 10:37:01 +0530

    lint test 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,30 @@
+// eslint.config.js
 export default [
-    {
+  {
       files: ["**/*.js"],
-      rules: {
-        // Possible Errors
-        "no-debugger": "error",
-        "no-extra-semi": "error",
-        "no-irregular-whitespace": "error",
-        "no-unsafe-negation": "error",
-        
-        "curly": "error",
-        "eqeqeq": "error",
-        "no-eval": "error",
-        "no-implicit-globals": "error",
-        "no-multi-spaces": "error",
-        "no-new": "error",
-        "no-unused-expressions": "error",
-  
-        // Variables
-        "no-undef": "error",
-        "no-unused-vars": "error",
-        "no-use-before-define": "error",
-  
+      env: {
+          node: true,       // This makes ESLint recognize Node.js global variables
+          mocha: true,      // This makes ESLint recognize Mocha global variables
       },
-    },
-  ];
-  
+      rules: {
+          // Possible Errors
+          "no-debugger": "error",
+          "no-extra-semi": "error",
+          "no-irregular-whitespace": "error",
+          "no-unsafe-negation": "error",
+
+          "curly": "error",
+          "eqeqeq": "error",
+          "no-eval": "error",
+          "no-implicit-globals": "error",
+          "no-multi-spaces": "error",
+          "no-new": "error",
+          "no-unused-expressions": "error",
+
+          // Variables
+          "no-undef": "error",
+          "no-unused-vars": "error",
+          "no-use-before-define": "error",
+      },
+  },
+];
