e12ce6159c94cecf2c7fe66d9e0bddd0ed20afcd
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-18 10:45:27 +0530

    lint check 5

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,33 @@
-// eslint.config.js
 export default [
   {
-      files: ["**/*.js"],
-      languageOptions: {
-        env: {
-            node: true,    // Recognize Node.js global variables
-            mocha: true,   // Recognize Mocha global variables
-        },
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
     },
-      rules: {
-          // Possible Errors
-          "no-debugger": "error",
-          "no-extra-semi": "error",
-          "no-irregular-whitespace": "error",
-          "no-unsafe-negation": "error",
+    env: {
+      node: true,    // Recognize Node.js global variables
+      mocha: true,   // Recognize Mocha global variables
+    },
+    rules: {
+      // Possible Errors
+      "no-debugger": "error",
+      "no-extra-semi": "error",
+      "no-irregular-whitespace": "error",
+      "no-unsafe-negation": "error",
 
-          "curly": "error",
-          "eqeqeq": "error",
-          "no-eval": "error",
-          "no-implicit-globals": "error",
-          "no-multi-spaces": "error",
-          "no-new": "error",
-          "no-unused-expressions": "error",
+      "curly": "error",
+      "eqeqeq": "error",
+      "no-eval": "error",
+      "no-implicit-globals": "error",
+      "no-multi-spaces": "error",
+      "no-new": "error",
+      "no-unused-expressions": "error",
 
-          // Variables
-          "no-undef": "error",
-          "no-unused-vars": "error",
-          "no-use-before-define": "error",
-      },
+      // Variables
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-use-before-define": "error",
+    },
   },
 ];
