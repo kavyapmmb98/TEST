9935bb60556f267202fcdfac3aeae971ddb5ad44
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-18 11:03:26 +0530

    lint test 6

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,8 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-    env: {
-      node: true,    // Recognize Node.js global variables
-      mocha: true,   // Recognize Mocha global variables
-    },
-    rules: {
-      // Possible Errors
-      "no-debugger": "error",
-      "no-extra-semi": "error",
-      "no-irregular-whitespace": "error",
-      "no-unsafe-negation": "error",
-
-      "curly": "error",
-      "eqeqeq": "error",
-      "no-eval": "error",
-      "no-implicit-globals": "error",
-      "no-multi-spaces": "error",
-      "no-new": "error",
-      "no-unused-expressions": "error",
-
-      // Variables
-      "no-undef": "error",
-      "no-unused-vars": "error",
-      "no-use-before-define": "error",
-    },
-  },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
