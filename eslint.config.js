6d7b36f56de74c3b83c71f8edb6c3e90ca2a59a2
kavyapmmb98 <kavyapmmb@gmail.com>
2024-07-18 14:36:08 +0530

    lint final

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,28 @@
+// Import necessary dependencies
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+// Define custom globals for Mocha
+const mochaGlobals = {
+    describe: "readonly",
+    beforeEach: "readonly",
+    afterEach: "readonly",
+    it: "readonly",
+    after: "readonly",
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+};
+
+const combinedConfig = [
+    // Maintain existing configuration with additional Mocha globals
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser, 
+                ...mochaGlobals,   
+            }
+        }
+    },
+    pluginJs.configs.recommended
 ];
+
+export default combinedConfig;
